0d055f23bf54d4044a053de3db8e74427065cf9c
magibeg <ian.dublack@gmail.com>
2019-02-16 22:27:22 -0500

    added dictionary.py

---
+++ Random Short Programs/Dictionary.py
@@ -0,0 +1,15 @@
+#Playing around with dictionaries
+
+#populate some data
+carData = {"Make": "Honda", "Model": "Accord", "Trim": "EX-L", "Colour": "Black"}
+
+#print the data out
+print(carData["Make"])
+print(carData["Model"])
+print(carData["Trim"])
+
+#change the colour
+carData["Colour"] = "Blue"
+
+#print the new colour
+print(carData["Colour"])
