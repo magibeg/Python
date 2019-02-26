1b499acbe712f9f9d4569e8a6509721d5f701300
magibeg <ian.dublack@gmail.com>
2019-02-26 14:27:21 -0500

    corrected formatting

--- Random Short Programs/firstGame.py
+++ Random Short Programs/firstGame.py
@@ -36,7 +36,7 @@ while run:
     if keys[pygame.K_LEFT]:
         x -= vel
     if keys[pygame.K_RIGHT]:
-        x+= vel
+        x += vel
     if keys[pygame.K_UP]:
         y -= vel
     if keys[pygame.K_DOWN]:
